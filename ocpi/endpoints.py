84928612a54fe1f606a3cdebe571a2e99d3cd412
HAkhavan71 <hakh.27@gmail.com>
2022-07-15 16:41:50 +0430

    feat: add versions details endpoint

---
+++ ocpi/endpoints.py
@@ -0,0 +1,14 @@
+from ocpi.core.enums import ModuleID
+from ocpi.core.data_types import URL
+from ocpi.core.config import settings
+from ocpi.versions.schemas import Endpoint
+from ocpi.versions.enums import VersionNumber, InterfaceRole
+
+ENDPOINTS = [
+    Endpoint(
+        identifier=ModuleID.Locations,
+        role=InterfaceRole.Receiver,
+        url=URL(f'https://{settings.HOST}/cpo/{VersionNumber._2_2_1}/{ModuleID.Locations}')
+    ),
+
+]
--- ocpi/versions/api/cpo/v2_2_1.py
+++ ocpi/versions/api/cpo/v2_2_1.py
@@ -1,22 +1,32 @@
 from fastapi import APIRouter
 
-from versions.schemas import Version, URL
+from versions.schemas import Version, VersionDetail, URL
 from versions.enums import VersionNumber
 from ocpi.core.schemas import OCPIResponse
 from ocpi.core.config import settings
+from ocpi.endpoints import ENDPOINTS
 from ocpi.core import status
 
-router = APIRouter(
-    prefix='/versions',
-)
+router = APIRouter()
 
 
-@router.get("/", response_model=OCPIResponse)
+@router.get("/versions", response_model=OCPIResponse)
 def get_versions():
     return OCPIResponse(
         data=Version(
             version=VersionNumber._2_2_1,
-            url=URL(f'{settings.HOST}/cpo/2.2.1')
+            url=URL(f'https://{settings.HOST}/cpo/2.2.1')
+        ).dict(),
+        **status.OCPI_1000_GENERIC_SUCESS_CODE,
+    )
+
+
+@router.get("/2.2.1/details", response_model=OCPIResponse)
+def get_version_details():
+    return OCPIResponse(
+        data=VersionDetail(
+            version=VersionNumber._2_2_1,
+            endpoints=ENDPOINTS
         ).dict(),
         **status.OCPI_1000_GENERIC_SUCESS_CODE,
     )
