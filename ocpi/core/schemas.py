66b761dd34a310c2cbfcd7074d6bd97e51dd7ff9
HAkhavan71 <hakh.27@gmail.com>
2022-07-25 14:44:51 +0430

    feat: make crud and adapter dependency, add example

---
+++ ocpi/core/schemas.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timezone
+
+from pydantic import BaseModel
+
+from ocpi.core.data_types import String, DateTime
+
+
+class OCPIResponse(BaseModel):
+    data: list
+    status_code: int
+    status_message: String
+    timestamp: DateTime = str(datetime.now(timezone.utc))
