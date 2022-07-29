b01a4463bbf39830c01f47ac5304775fce7fae89
HAkhavan71 <hakh.27@gmail.com>
2022-07-29 10:54:20 +0430

    refactor: make routers and CRUD methods asycn

--- ocpi/crud.py
+++ ocpi/crud.py
@@ -3,23 +3,23 @@ from ocpi.core.enums import ModuleID
 
 class CRUD:
     @classmethod
-    def get(cls, module: ModuleID, id):
+    async def get(cls, module: ModuleID, id):
         pass
 
     @classmethod
-    def list(cls, module: ModuleID) -> list:
+    async def list(cls, module: ModuleID) -> list:
         pass
 
     @classmethod
-    def create(cls, module: ModuleID, data):
+    async def create(cls, module: ModuleID, data):
         pass
 
     @classmethod
-    def update(cls, module: ModuleID, data):
+    async def update(cls, module: ModuleID, data):
         pass
 
     @classmethod
-    def delete(cls, module: ModuleID, id):
+    async def delete(cls, module: ModuleID, id):
         pass
 
 
