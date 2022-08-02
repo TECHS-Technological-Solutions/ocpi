1b53bf42286cef44d6240eaf074bc4f1b927a347
ViCt0r99 <wiktor.jan999@gmail.com>
2022-08-02 15:58:27 +0200

    feat: implement basic cli app

---
+++ ocpi/cli.py
@@ -0,0 +1,43 @@
+import random
+import typer
+import questionary
+
+app = typer.Typer()
+
+
+@app.command()
+def start():
+    program_continue = typer.confirm("You have just started OCPI app, do you want to continue")
+    if not program_continue:
+        raise typer.Abort()
+
+    # Charge point information
+    cp_serial_number = typer.prompt("Enter charge point serial number")
+    central_system_url = typer.prompt("Enter central system URL")
+
+    # Connection mocking
+    string = "You are connecting to %s charge point via %s"
+    typer.echo(string % (cp_serial_number, central_system_url))
+    # TODO: Random function is just to check functionality of typer command, later we will implement real connection
+    connection = random.choice([True, False])
+    if not connection:
+        typer.echo('Connection was unsuccessful. In order to try again you need to call start command.')
+        raise typer.Abort()
+
+    answer = questionary.select(
+        "What action do you want to perform: ",
+        choices=[
+            'Quit',
+            'Send an OCPP message'
+        ]
+    ).ask()
+
+    if answer == 'Quit':
+        raise typer.Abort()
+
+    ocpp_message = typer.prompt('Enter your OCPP message')
+    typer.echo(ocpp_message)
+
+
+if __name__ == '__main__':
+    app()
