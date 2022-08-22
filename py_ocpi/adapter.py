1a53ec541ebc223a24c9b79a042437cf7004d8d6
Hamed Akhavan <61178707+HAkhavan71@users.noreply.github.com>
2022-08-22 10:44:51 +0430

    OCPI-55 (#7)
    * feat: add sessions module schemas and enums
    
    * feat: add sessions module schemas and enums
    
    * feat: add sessions endpoints

--- py_ocpi/adapter.py
+++ py_ocpi/adapter.py
@@ -1,10 +1,17 @@
 from py_ocpi.locations.v_2_2_1.schemas import Location
+from py_ocpi.sessions.v_2_2_1.enums import ChargingPreferencesResponse
 
 
 class Adapter:
     @classmethod
     def location_adapter(cls, data) -> Location:
-        pass
+        ...
+
+    def session_adapter(cls, data) -> Location:
+        ...
+
+    def charging_preference_adapter(cls, data) -> ChargingPreferencesResponse:
+        ...
 
 
 def get_adapter():
---
+++ py_ocpi/modules/cdrs/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class CdrToken(BaseModel):
+    ...
+
+
+class ChargingPeriod(BaseModel):
+    ...
