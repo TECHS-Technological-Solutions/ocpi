806c99783100cf041c6de46e078767bede7b9083
Hamed Akhavan <61178707+HAkhavan71@users.noreply.github.com>
2022-08-22 10:44:51 +0430

    OCPI-55 (#7)
    * feat: add sessions module schemas and enums
    
    * feat: add sessions module schemas and enums
    
    * feat: add sessions endpoints

--- py_ocpi/endpoints.py
+++ py_ocpi/endpoints.py
@@ -13,6 +13,13 @@ ENDPOINTS = {
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
                     f'/{VersionNumber.v_2_2_1}/{ModuleID.Locations}')
         ),
+        # sessions
+        Endpoint(
+            identifier=ModuleID.Sessions,
+            role=InterfaceRole.Sender,
+            url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
+                    f'/{VersionNumber.v_2_2_1}/{ModuleID.Sessions}')
+        ),
     ]
 
 }
