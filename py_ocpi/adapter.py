66309d598719252a3efbd3fe0fd91dd70fefd435
ViCt0r99 <87501969+ViCt0r99@users.noreply.github.com>
2022-08-23 12:45:29 +0200

    OCPI-48 (#3)
    * feat: add schemas and enums for credentials module
    
    * fix: pylint fix
    
    * feat: add credentials module
    
    * fix: PR review changes
    
    * feat: make crud funtions in credentials endpoint async
    
    * fix: change verification params

--- py_ocpi/adapter.py
+++ py_ocpi/adapter.py
@@ -1,4 +1,5 @@
 from py_ocpi.locations.v_2_2_1.schemas import Location
+from py_ocpi.credentials.v_2_2_1.schemas import Credentials
 from py_ocpi.sessions.v_2_2_1.enums import ChargingPreferencesResponse
 
 
@@ -12,6 +13,10 @@ class Adapter:
 
     def charging_preference_adapter(cls, data) -> ChargingPreferencesResponse:
         ...
+
+    @classmethod
+    def credentials_adapter(cls, data) -> Credentials:
+        pass
 
 
 def get_adapter():
