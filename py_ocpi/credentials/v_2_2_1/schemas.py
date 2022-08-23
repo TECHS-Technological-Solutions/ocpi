8fd2247cf4b01cbe3e27dc580bc62b8c359c5375
ViCt0r99 <87501969+ViCt0r99@users.noreply.github.com>
2022-08-23 12:45:29 +0200

    OCPI-48 (#3)
    * feat: add schemas and enums for credentials module
    
    * fix: pylint fix
    
    * feat: add credentials module
    
    * fix: PR review changes
    
    * feat: make crud funtions in credentials endpoint async
    
    * fix: change verification params

--- py_ocpi/credentials/v_2_2_1/schemas.py
+++ py_ocpi/credentials/v_2_2_1/schemas.py
@@ -1,9 +1,11 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 
 from py_ocpi.core.data_types import CiString, URL, String
 from py_ocpi.core.enums import RoleEnum
 from py_ocpi.locations.v_2_2_1.schemas import BusinessDetails
+from py_ocpi.versions.schemas import Version
+from py_ocpi.endpoints import ENDPOINTS
 
 
 class CredentialsRole(BaseModel):
@@ -23,3 +25,16 @@ class Credentials(BaseModel):
     token: String(64)
     url: URL
     roles: List[CredentialsRole]
+
+
+class ServerCredentials(BaseModel):
+    # Client's B token
+    cred_token_b: String(max_length=64)
+    # Client's versions
+    versions: Version
+    # Client's endpoints
+    endpoints: ENDPOINTS
+    # Server versions URL
+    url: Optional[URL]
+    # Server business details
+    roles: Optional[List[CredentialsRole]]
