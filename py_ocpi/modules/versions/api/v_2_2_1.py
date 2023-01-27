cf817962f8b4925354a27027578ede10fdb036e3
Wojtek Siudzinski <admin@suda.pl>
2023-01-27 18:43:44 +0100

    Split endpoints by role

--- py_ocpi/modules/versions/api/v_2_2_1.py
+++ py_ocpi/modules/versions/api/v_2_2_1.py
@@ -5,17 +5,22 @@ from py_ocpi.modules.versions.enums import VersionNumber
 from py_ocpi.core import status
 from py_ocpi.core.schemas import OCPIResponse
 from py_ocpi.core.endpoints import ENDPOINTS
+from py_ocpi.core.enums import RoleEnum
 
 router = APIRouter()
 
 
 @router.get("/2.2.1/details", response_model=OCPIResponse)
 async def get_version_details():
+    cpo_endpoints = ENDPOINTS[VersionNumber.v_2_2_1][RoleEnum.cpo]
+    emps_endpoints = ENDPOINTS[VersionNumber.v_2_2_1][RoleEnum.emsp]
+    endpoints = cpo_endpoints + emps_endpoints
+
     return OCPIResponse(
         data=[
             VersionDetail(
                 version=VersionNumber.v_2_2_1,
-                endpoints=ENDPOINTS[VersionNumber.v_2_2_1]
+                endpoints=endpoints
             ).dict(),
         ],
         **status.OCPI_1000_GENERIC_SUCESS_CODE,
