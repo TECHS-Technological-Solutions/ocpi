e9d5429e89d423322c0e0f7ba9b75e3c40d213e6
Wojtek Siudzinski <admin@suda.pl>
2023-01-29 13:58:12 +0100

    Fix the router URLs (#36)
    * Fix the router URLs
    
    * Fix versions and details URLs

--- py_ocpi/core/endpoints.py
+++ py_ocpi/core/endpoints.py
@@ -13,42 +13,42 @@ ENDPOINTS = {
             identifier=ModuleID.locations,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.locations}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.locations.value}')
         ),
         # sessions
         Endpoint(
             identifier=ModuleID.sessions,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.sessions}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.sessions.value}')
         ),
         # credentials
         Endpoint(
             identifier=ModuleID.credentials_and_registration,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.credentials_and_registration}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.credentials_and_registration.value}')
         ),
         # tariffs
         Endpoint(
             identifier=ModuleID.tariffs,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.tariffs}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.tariffs.value}')
         ),
         # cdrs
         Endpoint(
             identifier=ModuleID.cdrs,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.cdrs}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.cdrs.value}')
         ),
         # tokens
         Endpoint(
             identifier=ModuleID.tokens,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/cpo'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.tokens}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.tokens.value}')
         ),
 
         # ###############--EMSP--###############
@@ -58,49 +58,49 @@ ENDPOINTS = {
             identifier=ModuleID.credentials_and_registration,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.credentials_and_registration}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.credentials_and_registration.value}')
         ),
         # locations
         Endpoint(
             identifier=ModuleID.locations,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.locations}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.locations.value}')
         ),
         # sessions
         Endpoint(
             identifier=ModuleID.sessions,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.sessions}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.sessions.value}')
         ),
         # cdrs
         Endpoint(
             identifier=ModuleID.cdrs,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.cdrs}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.cdrs.value}')
         ),
         # tariffs
         Endpoint(
             identifier=ModuleID.tariffs,
             role=InterfaceRole.receiver,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.tariffs}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.tariffs.value}')
         ),
         # commands
         Endpoint(
             identifier=ModuleID.commands,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.commands}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.commands.value}')
         ),
         # tokens
         Endpoint(
             identifier=ModuleID.tokens,
             role=InterfaceRole.sender,
             url=URL(f'https://{settings.OCPI_HOST}/{settings.OCPI_PREFIX}/emsp'
-                    f'/{VersionNumber.v_2_2_1}/{ModuleID.tokens}')
+                    f'/{VersionNumber.v_2_2_1.value}/{ModuleID.tokens.value}')
         ),
     ]
 
