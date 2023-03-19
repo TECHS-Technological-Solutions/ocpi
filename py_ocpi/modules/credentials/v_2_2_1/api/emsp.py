29f769a0991081d7c99608e9d76503d30edc6cbf
Wojtek Siudzinski <admin@suda.pl>
2023-03-19 07:18:09 +0100

    Fix initial credentials exchange (#46)
    * Fix the initial authentication (#43)
    
    * Base64 encode the bearer token
    
    * Decode the base64 encoded authentication token
    
    * Require authorization when listing versions
    
    * Remove the list wrapping the credentials object
    
    * Fix tests
    
    * Skip false positive Bandit test B105
    
    * Ignore false positive
    
    * Fix linter error

--- py_ocpi/modules/credentials/v_2_2_1/api/emsp.py
+++ py_ocpi/modules/credentials/v_2_2_1/api/emsp.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request, status as fastap
 from py_ocpi.core.schemas import OCPIResponse
 from py_ocpi.core.adapter import Adapter
 from py_ocpi.core.crud import Crud
-from py_ocpi.core.utils import get_auth_token
+from py_ocpi.core.utils import encode_string_base64, get_auth_token
 from py_ocpi.core.dependencies import get_crud, get_adapter
 from py_ocpi.core import status
 from py_ocpi.core.enums import Action, ModuleID, RoleEnum
@@ -24,7 +24,7 @@ async def get_credentials(request: Request, crud: Crud = Depends(get_crud), adap
     data = await crud.get(ModuleID.credentials_and_registration, RoleEnum.emsp,
                           auth_token, auth_token=auth_token, version=VersionNumber.v_2_2_1)
     return OCPIResponse(
-        data=[adapter.credentials_adapter(data).dict()],
+        data=adapter.credentials_adapter(data).dict(),
         **status.OCPI_1000_GENERIC_SUCESS_CODE,
     )
 
@@ -43,7 +43,7 @@ async def post_credentials(request: Request, credentials: Credentials,
 
     # Retrieve the versions and endpoints from the client
     async with httpx.AsyncClient() as client:
-        authorization_token = f'Token {credentials_client_token}'
+        authorization_token = f'Token {encode_string_base64(credentials_client_token)}'
         response_versions = await client.get(credentials.url,
                                              headers={'authorization': authorization_token})
 
@@ -78,7 +78,7 @@ async def post_credentials(request: Request, credentials: Credentials,
                 )
 
                 return OCPIResponse(
-                    data=[adapter.credentials_adapter(new_credentials).dict()],
+                    data=adapter.credentials_adapter(new_credentials).dict(),
                     **status.OCPI_1000_GENERIC_SUCESS_CODE
                 )
 
@@ -102,7 +102,7 @@ async def update_credentials(request: Request, credentials: Credentials,
 
     # Retrieve the versions and endpoints from the client
     async with httpx.AsyncClient() as client:
-        authorization_token = f'Token {credentials_client_token}'
+        authorization_token = f'Token {encode_string_base64(credentials_client_token)}'
         response_versions = await client.get(credentials.url, headers={'authorization': authorization_token})
 
         if response_versions.status_code == fastapistatus.HTTP_200_OK:
@@ -134,7 +134,7 @@ async def update_credentials(request: Request, credentials: Credentials,
                                                     version=VersionNumber.v_2_2_1)
 
                 return OCPIResponse(
-                    data=[adapter.credentials_adapter(new_credentials).dict()],
+                    data=adapter.credentials_adapter(new_credentials).dict(),
                     **status.OCPI_1000_GENERIC_SUCESS_CODE
                 )
 
--- tests/test_modules/test_credentials.py
+++ tests/test_modules/test_credentials.py
@@ -1,6 +1,7 @@
 import functools
 from uuid import uuid4
 from unittest.mock import patch
+from typing import Any
 
 import pytest
 from fastapi.testclient import TestClient
@@ -11,6 +12,7 @@ from py_ocpi.core import enums
 from py_ocpi.core.data_types import URL
 from py_ocpi.core.config import settings
 from py_ocpi.core.dependencies import get_versions
+from py_ocpi.core.utils import encode_string_base64
 from py_ocpi.modules.credentials.v_2_2_1.schemas import Credentials
 from py_ocpi.modules.tokens.v_2_2_1.enums import AllowedType
 from py_ocpi.modules.tokens.v_2_2_1.schemas import AuthorizationInfo, Token
@@ -80,27 +82,31 @@ def test_cpo_get_credentials_v_2_2_1():
     app = get_application(VersionNumber.v_2_2_1, [enums.RoleEnum.cpo], Crud, Adapter)
     token = str(uuid4())
     header = {
-        "Authorization": f'Token {token}'
+        "Authorization": f'Token {encode_string_base64(token)}'
     }
 
     client = TestClient(app)
     response = client.get('/ocpi/cpo/2.2.1/credentials', headers=header)
 
     assert response.status_code == 200
-    assert len(response.json()['data']) == 1
-    assert response.json()['data'][0]['token'] == token
+    assert response.json()['data']['token'] == token
 
 
 @pytest.mark.asyncio
 @patch('py_ocpi.modules.credentials.v_2_2_1.api.cpo.httpx.AsyncClient')
 async def test_cpo_post_credentials_v_2_2_1(async_client):
-    app_1 = get_application(VersionNumber.v_2_2_1, [enums.RoleEnum.emsp], Crud, Adapter)
+    class MockCrud(Crud):
+        @classmethod
+        async def do(cls, module: enums.ModuleID, role: enums.RoleEnum, action: enums.Action, auth_token, *args, data: dict = None, **kwargs) -> Any:
+            return {}
+
+    app_1 = get_application(VersionNumber.v_2_2_1, [enums.RoleEnum.emsp], MockCrud, Adapter)
 
     def override_get_versions():
         return [
             Version(
                 version=VersionNumber.v_2_2_1,
-                url=URL(f'/{settings.OCPI_PREFIX}/{VersionNumber.v_2_2_1}/details')
+                url=URL(f'/{settings.OCPI_PREFIX}/{VersionNumber.v_2_2_1.value}/details')
             ).dict()
         ]
 
@@ -108,11 +114,11 @@ async def test_cpo_post_credentials_v_2_2_1(async_client):
 
     async_client.return_value = AsyncClient(app=app_1, base_url="http://test")
 
-    app_2 = get_application(VersionNumber.v_2_2_1, [enums.RoleEnum.cpo], Crud, Adapter)
+
+    app_2 = get_application(VersionNumber.v_2_2_1, [enums.RoleEnum.cpo], MockCrud, Adapter)
 
     async with AsyncClient(app=app_2, base_url="http://test") as client:
         response = await client.post('/ocpi/cpo/2.2.1/credentials/', json=CREDENTIALS_TOKEN_CREATE)
 
     assert response.status_code == 200
-    assert len(response.json()['data']) == 1
-    assert response.json()['data'][0]['token'] == CREDENTIALS_TOKEN_CREATE['token']
+    assert response.json()['data']['token'] == CREDENTIALS_TOKEN_CREATE['token']
